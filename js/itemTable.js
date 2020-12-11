4055bae05445729fdc192bc913f1219a8b0e8c26
Neto Rodrigues <jrodrigueson@gmail.com>
2020-12-11 09:07:54 -0300

    innitial commit

---
+++ js/itemTable.js
@@ -0,0 +1,28 @@
+//define data array
+var tabledata = [
+    { id: 1, name: "Oli Bob", progress: 12, gender: "male", rating: 1, col: "red", dob: "19/02/1984", car: 1 },
+    { id: 2, name: "Mary May", progress: 1, gender: "female", rating: 2, col: "blue", dob: "14/05/1982", car: true },
+    { id: 3, name: "Christine Lobowski", progress: 42, gender: "female", rating: 0, col: "green", dob: "22/05/1982", car: "true" },
+    { id: 4, name: "Brendon Philips", progress: 100, gender: "male", rating: 1, col: "orange", dob: "01/08/1980" },
+    { id: 5, name: "Margret Marmajuke", progress: 16, gender: "female", rating: 5, col: "yellow", dob: "31/01/1999" },
+    { id: 6, name: "Frank Harbours", progress: 38, gender: "male", rating: 4, col: "red", dob: "12/05/1966", car: 1 },
+];
+
+//initialize table
+var table = new Tabulator("#example-table", {
+    data: tabledata,           //load row data from array
+    layout: "fitColumns",      //fit columns to width of table
+    responsiveLayout: "hide",  //hide columns that dont fit on the table
+    tooltips: true,            //show tool tips on cells
+    addRowPos: "top",          //when adding a new row, add it to the top of the table
+    history: true,             //allow undo and redo actions on the table
+    pagination: "local",
+    paginationSize: 6,       //paginate the data
+    paginationSizeSelector: [3, 6, 8, 10],
+    movableColumns: true,      //allow column order to be changed
+    resizableRows: true,       //allow row order to be changed
+    initialSort: [             //set the initial sort order of the data
+        { column: "name", dir: "asc" },
+    ],         //set the initial sort order of the data
+    autoColumns: true, //create columns from data field names
+});
